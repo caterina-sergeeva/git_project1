9644c34125ed3e9e36f6077903eef602e7d025b8
Ekaterina <ekatherina20@gmail.com>
2021-12-27 18:56:21 +0300

    я внесла изменения

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def my_superfunction():
 def main():
     print('My first git program')
     print('And I change it every day')
-    print('Again')
+    print('Again new version')
     print('UFO came and added this line')
     my_superfunction()
 
